1d8da492296ea1264d06bc404d9a6efc5b3c4241
HYPech <TCTORBAO@GMAIL.COM>
2021-02-06 21:41:18 -0500

    Update PureTone.java

--- app/src/main/java/com/seeingvoice/case36audiotrack_puretone/PureTone.java
+++ app/src/main/java/com/seeingvoice/case36audiotrack_puretone/PureTone.java
@@ -5,22 +5,9 @@ import static com.seeingvoice.case36audiotrack_puretone.GlobalConfig.AMPLITUDE;
 
 public class PureTone {
 
-	/**
-	 * 生成正弦波
-	 * 
-	 * @param wave //array of value of sin()
-	 * @param increment //step
-	 *
-	 * @return
-	 */
-
 	public static short[] sine(short[] wave, double increment) {
 		for (int i = 0; i < SAMPLE_RATE; i++) {
-
 			wave[i] = (short) (AMPLITUDE * Math.sin(increment * i));
-
-//            wave[i] = (short) (HEIGHT * (1 - Math.sin(TWOPI * ((i % waveLen) * 1.00 / waveLen))));
-//            wave[i] = (short) (HEIGHT * (1 - Math.sin(TWOPI * ((i % waveLen) * 1.00 / waveLen))));
 		}
 		return wave;
 	}
